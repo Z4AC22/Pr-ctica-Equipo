3ba78a3ceaa58f559998d3e635e86459c4947f6a
unknown <zaac202211@gmail.com>
2024-02-28 18:34:15 -0600

    termine

--- test28.py
+++ test28.py
@@ -1,4 +1,22 @@
-#instrucciones:
-#1. Te pida tus datos como nombre apelidos y edad
-#2. cree una curp con los datos ... en realidad son solo la inicial de tu nombre, tu edad y la inicial de tu apellido
-#3. agregar cualquier funcion
+nom=input('Escribe tu nombre: ')
+nom_upper = nom.upper()
+
+ap=input('Ingresa tu apellido: ')
+ap_upper = ap.upper()
+
+edad=input('Cuantos años tienes? ')
+
+x= nom_upper[0: 1]
+y=ap_upper[0: 1]
+curp= x+edad+y
+print(nom_upper,ap_upper,edad,curp)
+
+print('tu usuario es: ',curp)
+
+crea_contra=input('crea una contraseña: ')
+
+contra=input('ingresa tu contraseña: ')
+if contra==crea_contra:
+ print('bienvenido',curp)
+else:
+ print('tu contraseña es incorrecta')
